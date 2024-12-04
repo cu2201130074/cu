5af7fa1bd6c8a530770792809318c31838325e82
cu2201130074 <190558482+cu2201130074@users.noreply.github.com>
2024-12-04 23:06:21 +0900

    imple cat.js

---
+++ myapp/routes/cat.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+
+const cors = require('cors');
+router.use(cors());
+
+router.get('/', async (req, res) => {
+    request('https://dog.ceo/api/breeds/image/random', function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            const data = JSON.parse(body);
+            res.json(data);
+        }
+    });
+})
+
+module.exports = router;
